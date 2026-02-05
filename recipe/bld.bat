064d755298a262511d4a34355999bd214b10b758
Andrii Osipov <aosipov@anaconda.com>
2026-02-05 04:43:43 -0800

    try one more time with Y

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,12 @@
 @REM https://bugreports.qt.io/browse/QTBUG-107009
 set "PATH=%SRC_DIR%\build\lib\qt6\bin;%PATH%"
-::subst Y: "%SRC_DIR%"
-set BUILD_DIR=%SystemDrive%\qtdecl_%PKG_VERSION%
-if exist "%BUILD_DIR%" (
-    cd /d "%SystemDrive%\"
-    rmdir /s /q "%BUILD_DIR%"
-)
-mkdir "%BUILD_DIR%"
-if errorlevel 1 exit 1
+subst Y: "%SRC_DIR%"
 
-::cmake --log-level STATUS -S"Y:\%PKG_NAME%" -B"Y:\build" -GNinja ^
-cmake --log-level STATUS -S"%SRC_DIR%\%PKG_NAME%" -B"%BUILD_DIR%" -GNinja ^
+cmake --log-level STATUS -S"Y:\%PKG_NAME%" -B"Y:\build" -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_OBJECT_PATH_MAX=200 ^
     -DINSTALL_BINDIR=lib/qt6/bin ^
     -DINSTALL_PUBLICBINDIR=bin ^
     -DINSTALL_LIBEXECDIR=lib/qt6 ^
@@ -26,7 +19,7 @@ cmake --log-level STATUS -S"%SRC_DIR%\%PKG_NAME%" -B"%BUILD_DIR%" -GNinja ^
     -DINSTALL_DATADIR=share/qt6
 if errorlevel 1 exit 1
 
-cmake --build "%BUILD_DIR%" --target install
+cmake --build build --target install
 if errorlevel 1 exit 1
 
 xcopy /y /s %LIBRARY_PREFIX%\lib\qt6\bin\*.dll %LIBRARY_PREFIX%\bin
@@ -41,9 +34,5 @@ if errorlevel 1 exit 1
 copy %LIBRARY_PREFIX%\lib\qt6\bin\qmleasing.exe %LIBRARY_PREFIX%\bin\qmleasing6.exe
 if errorlevel 1 exit 1
 
-::cd %SRC_DIR%
-::subst Y: /D
-if exist "%BUILD_DIR%" (
-    cd /d "%SystemDrive%\"
-    rmdir /s /q "%BUILD_DIR%"
-)
+cd %SRC_DIR%
+subst Y: /D
