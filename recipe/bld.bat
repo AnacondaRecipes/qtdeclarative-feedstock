3f0cffdf4b0abe2f67757d85f611631699da4558
Andrii Osipov <aosipov@anaconda.com>
2026-02-05 05:27:24 -0800

    can`t build on Y, with cmake max path restriction

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,12 @@
 @REM https://bugreports.qt.io/browse/QTBUG-107009
-set "PATH=%SRC_DIR%\build\lib\qt6\bin;%PATH%"
-subst Y: "%SRC_DIR%"
+@REM Short build path on C: to avoid Windows path length limits on CI
+set "BUILD_DIR=%SystemDrive%\qtdecl_%PKG_VERSION%"
+set "PATH=%BUILD_DIR%\lib\qt6\bin;%PATH%"
 
-cmake --log-level STATUS -S"Y:\%PKG_NAME%" -B"Y:\build" -GNinja ^
+cmake --log-level STATUS -S"%SRC_DIR%\%PKG_NAME%" -B"%BUILD_DIR%" -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    -DCMAKE_OBJECT_PATH_MAX=200 ^
     -DINSTALL_BINDIR=lib/qt6/bin ^
     -DINSTALL_PUBLICBINDIR=bin ^
     -DINSTALL_LIBEXECDIR=lib/qt6 ^
@@ -19,7 +19,7 @@ cmake --log-level STATUS -S"Y:\%PKG_NAME%" -B"Y:\build" -GNinja ^
     -DINSTALL_DATADIR=share/qt6
 if errorlevel 1 exit 1
 
-cmake --build build --target install
+cmake --build "%BUILD_DIR%" --target install
 if errorlevel 1 exit 1
 
 xcopy /y /s %LIBRARY_PREFIX%\lib\qt6\bin\*.dll %LIBRARY_PREFIX%\bin
@@ -34,5 +34,4 @@ if errorlevel 1 exit 1
 copy %LIBRARY_PREFIX%\lib\qt6\bin\qmleasing.exe %LIBRARY_PREFIX%\bin\qmleasing6.exe
 if errorlevel 1 exit 1
 
-cd %SRC_DIR%
-subst Y: /D
+rd /s /q "%BUILD_DIR%" 2>nul
